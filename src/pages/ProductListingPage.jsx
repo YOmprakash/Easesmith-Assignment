e5a8ce1c3ee6a3946bc7ac171641da2d3ea3fe1a
YOmprakash <omprakashyamavaram@gmail.com>
2024-10-19 11:57:52 +0530

    filter section added

--- src/pages/ProductListingPage.jsx
+++ src/pages/ProductListingPage.jsx
@@ -3,34 +3,38 @@ import ProductCard from "../components/ProductCard";
 import mockProducts from "../data";
 import img91 from "../assets/image91.png";
 import { FiSearch } from "react-icons/fi";
-
+import FilterSection from '../components/FilterSection';
 const ProductListingPage = () => {
   return (
-    <div className="container mx-auto p-6 ">
+    <div className="container p-6 mx-auto ">
       {/* Search Bar Section */}
       <div className="flex items-center justify-center max-w-[500px] mx-auto mb-10 border-b-2">
         <FiSearch color="#838383" className="text-3xl " />
         <input
           type="text"
           placeholder="Search Plant"
-          className="w-full max-w-lg p-3   focus:outline-none"
+          className="w-full max-w-lg p-3 focus:outline-none"
         />
         <img src={img91} alt="Logo" className="w-10 ml-4" />
       </div>
 
       {/* Category Buttons */}
-      <div className="flex justify-start space-x-4 mb-8">
-        <button className="bg-[#165315]
-] text-white px-6 py-2 ">
+      <div className="flex justify-start mb-8 space-x-4">
+        <button
+          className="bg-[#165315]
+] text-white px-6 py-2 "
+        >
           Plants
         </button>
-        <button className="  text-black border px-6 py-2  hover:bg-blue-600">
+        <button className="px-6 py-2 text-black border hover:bg-blue-600">
           Pots
         </button>
       </div>
 
-      <p className="text-[#838383]
- text-left font-normal  leading-relaxed">
+      <p
+        className="text-[#838383]
+ text-left font-normal  leading-relaxed"
+      >
         Lorem ipsum dolor sit amet. Aut ipsam illum et nostrum quidem aut
         necessitatibus enim ut internos accusantium a numquam autem ab rerum
         omnis. Non molestiae ratione et laborum doloribus aut molestiae
@@ -41,14 +45,19 @@ const ProductListingPage = () => {
 
       {/* Slider Section Placeholder */}
       <div className="mb-10">
-        <p className="text-center text-lg text-gray-500 italic">*slider*</p>
+        <p className="text-lg italic text-center text-gray-500">*slider*</p>
       </div>
 
+      <div className="flex flex-col gap-6 md:flex-row">
+      <aside className="w-full p-4 bg-white border border-gray-200 rounded md:w-1/4">
+          <FilterSection />
+        </aside>
       {/* Product Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
         {mockProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
+      </div>
       </div>
     </div>
   );
